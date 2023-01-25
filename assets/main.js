f1f098e4965ecd1b99f0aea99046f4d5c477e295
ogawa69 <38399063+ogawa69@users.noreply.github.com>
2023-01-25 12:19:31 +0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 const searchLine = document.querySelector('.search__line')
-const dropList = document.querySelector('.drop-list')
+const dropList = document.querySelector('.search__drop-list')
 const selected = document.querySelector('.selected')
 
 function createElement(elementTag, elementClass) {
@@ -11,12 +11,15 @@ function createElement(elementTag, elementClass) {
 }
 
 function createSearchRes(userData) {
+
     const searchRes = document.createElement('li')
-    searchRes.classList.add('drop-list__item')
+    searchRes.classList.add('search__item')
     searchRes.textContent = userData.name
     dropList.append(searchRes)
 
     searchRes.addEventListener('click', () => {
+        searchLine.value = ''
+        clearResult()
         createSelectedItem(userData)
     })
 }
@@ -27,7 +30,7 @@ function createSelectedItem(userData) {
     const selectedInfo = document.createElement('div')
     selectedInfo.classList.add('selected__info')
     selectedInfo.textContent = 
-    `Name: ${userData.name};\nOwner: ${userData.owner.login};\nStars: ${userData.stargazers_count}`
+    `Name: ${userData.name}\nOwner: ${userData.owner.login}\nStars: ${userData.stargazers_count}`
     const removeBtn = document.createElement('button')
     removeBtn.src = './assets/close.svg'
     removeBtn.classList.add('selected__remove')
@@ -39,28 +42,31 @@ function createSelectedItem(userData) {
     })
 }
 
-async function gitApi(searchValue) {
-    return await fetch(`https://api.github.com/search/repositories?q=${searchValue}&per_page=5`).then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-    }).catch(err => console.log(err))
+const url = 'https://api.github.com/search/repositories'
+
+async function getRepositories(searchValue) {
+    try {
+        const response = await fetch(`${url}?q=${searchValue}&per_page=5`)
+        return response.json()
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
 
 async function searchUsers() {
     const searchValue = searchLine.value
-    if (searchValue) {
-        return gitApi(searchValue).then(res => {
-            clearResult()
-            res.items.forEach(el => createSearchRes(el));
-        })
-    } else {
+    if (!searchValue) {
+        return
+    }
+    getRepositories(searchValue).then(res => {
         clearResult()
-    }    
+        res.items.forEach(el => createSearchRes(el));
+    })
 }
 
 function clearResult() {
-    document.querySelectorAll('li').forEach(el => el.remove())
+    document.querySelectorAll('.search__item').forEach(el => el.remove())
 }
 
 function debounce(fn, debounceTime) {
@@ -75,8 +81,4 @@ function debounce(fn, debounceTime) {
 
 searchLine.addEventListener('keyup', function() {
     debounce(searchUsers.bind(this), 400).bind(this)()
-})
-
-dropList.addEventListener('click', function(event) {
-    
 })
